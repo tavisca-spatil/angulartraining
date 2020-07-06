5fe7ece525f2390e44c74b40981fd8d509c25399
spatil <spatil@tavisca.com>
2020-07-06 17:52:33 +0530

    added solution for assingment_01

---
+++ Fandamentals/assignment_01.js
@@ -0,0 +1,56 @@
+var phrase = 'first of all, just getting to the theater presents difficulties. leaving a home equipped with a tv and a video recorder is not an attractive idea on a humid, cold, or rainy night. even if the weather cooperates, there is still a thirty-minute drive to the theater down a congested highway, followed by the hassle of looking for a parking space. and then there are the lines. after hooking yourself to the end of a human chain, you worry about whether there will be enough tickets, whether you will get seats together, and whether many people will sneak into the line ahead of you.';
+function sentenceCase(str) {
+    if (str.length > 0) {
+        var result = str.toString().replace(/(^|\. *)([a-z])/g, function (match, separator, char) {
+            return "" + separator + char.toUpperCase();
+        });
+        console.log("String conversion in sentence case: '" + result + "' \n");
+    }
+    else {
+        console.log('Empty string. \n');
+    }
+}
+function wordCount(str, matchingStr) {
+    if (str.length > 0) {
+        var words = str.split(" ");
+        var count = void 0;
+        count = 0;
+        for (var i = 0; i < words.length - 1; i++) {
+            if (words[i] === matchingStr) {
+                count++;
+            }
+        }
+        console.log("Total words matching with '" + matchingStr + "' in the phrase are : " + count + " \n");
+    }
+    else {
+        console.log('Empty string. \n');
+    }
+}
+function totalWordCount(str) {
+    if (str.length > 0) {
+        var words = str.split(" ");
+        console.log("Total words in the phrase are: " + words.length + " \n");
+    }
+    else {
+        console.log('Empty string. \n');
+    }
+}
+function wordsWithChar(str, char) {
+    if (str.length > 0) {
+        var words = str.split(" ");
+        var wordsWithChar = '';
+        for (var i = 0; i < words.length - 1; i++) {
+            if (words[i].indexOf(char) !== -1) {
+                wordsWithChar += words[i] + ',';
+            }
+        }
+        console.log("Total words with '" + char + "' in the phrase are : " + wordsWithChar + " \n");
+    }
+    else {
+        console.log('Empty string. \n');
+    }
+}
+sentenceCase(phrase);
+wordsWithChar(phrase, 'a');
+totalWordCount(phrase);
+wordCount(phrase, 'the');
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# angulartraining
+# angular training assignments
+
+1: Create a TypeScript file that will do the following
+
+    a) Define a multi-statement String and perform following operations on it:
+        1) Convert a string in Statement Case (First character after . must be in upper case)
+        2) Print number of words in the string (string in between two blank spaces)
+        3) List all strings having character 'a' in it. (Hint: Use 'contains()' method)
+        4) Print number of matching Words in string. e.g. the word 'the' occurs 10 times.
